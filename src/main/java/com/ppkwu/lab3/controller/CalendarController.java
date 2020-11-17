e46f2c2a4b6d60ee74a256531fd0cede13692ef9
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-11-17 14:32:56 +0100

    Create GET endpoint and method for it

--- src/main/java/com/ppkwu/lab3/controller/CalendarController.java
+++ src/main/java/com/ppkwu/lab3/controller/CalendarController.java
@@ -1,7 +1,15 @@
 package com.ppkwu.lab3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalendarController {
+
+    @GetMapping("calendar/{year}/{month}")
+    public void buildCalendar(@PathVariable String year, @PathVariable String month) {
+        System.out.println("Year: " + year + ", Month: " + month);
+    }
+
 }
