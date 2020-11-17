7e5734d3b8a3825eb877aa5ae59c4e34673c0a13
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-11-17 15:02:13 +0100

    Generate example ics file, just to see if it works

--- src/main/java/com/ppkwu/lab3/controller/CalendarController.java
+++ src/main/java/com/ppkwu/lab3/controller/CalendarController.java
@@ -1,14 +1,38 @@
 package com.ppkwu.lab3.controller;
 
+import biweekly.Biweekly;
+import biweekly.ICalendar;
+import biweekly.component.VEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class CalendarController {
 
-    @GetMapping("calendar/{year}/{month}")
-    public void buildCalendar(@PathVariable String year, @PathVariable String month) {
+    @GetMapping(value = "calendar/{year}/{month}")
+    public void buildCalendar(@PathVariable String year, @PathVariable String month) throws IOException, ParseException {
+        ICalendar calendar = new ICalendar();
+        calendar.setExperimentalProperty("X-WR-CALNAME", "Spring Releases");
+
+        // event
+        VEvent event = new VEvent();
+        event.setSummary("Team Meeting");
+        Date start = new SimpleDateFormat("yyyy-MM-dd").parse("2020-11-18");
+        event.setDateStart(start);
+        Date end = new SimpleDateFormat("yyyy-MM-dd").parse("2020-11-18");
+        event.setDateEnd(end);
+        calendar.addEvent(event);
+
+        File ics = new File("conference.ics");
+        Biweekly.write(calendar).go(ics);
+
         System.out.println("Year: " + year + ", Month: " + month);
     }
 
