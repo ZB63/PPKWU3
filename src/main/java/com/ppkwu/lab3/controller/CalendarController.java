e34ef537272b6c388364fa010caa52db9ba11e72
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-11-17 15:51:24 +0100

    User can download example ics file

--- src/main/java/com/ppkwu/lab3/controller/CalendarController.java
+++ src/main/java/com/ppkwu/lab3/controller/CalendarController.java
@@ -3,12 +3,20 @@ package com.ppkwu.lab3.controller;
 import biweekly.Biweekly;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,7 +25,7 @@ import java.util.Date;
 public class CalendarController {
 
     @GetMapping(value = "calendar/{year}/{month}")
-    public void buildCalendar(@PathVariable String year, @PathVariable String month) throws IOException, ParseException {
+    public ResponseEntity buildCalendar(@PathVariable String year, @PathVariable String month) throws IOException, ParseException {
         ICalendar calendar = new ICalendar();
         calendar.setExperimentalProperty("X-WR-CALNAME", "Spring Releases");
 
@@ -33,7 +41,17 @@ public class CalendarController {
         File ics = new File("conference.ics");
         Biweekly.write(calendar).go(ics);
 
-        System.out.println("Year: " + year + ", Month: " + month);
+        Path path = Paths.get("conference.ics");
+        Resource resource = null;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
